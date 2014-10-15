6e95ebdae56a4c306e7a90aaa4ef9a54ce7bea62
Thorsten Lorenz <thlorenz@gmx.de>
2014-10-15 14:43:10 -0400

    devtools: adding console and network agent
    - at this point no more protocol errors are reported
    - instead certain messages are ignored and an empty result sent back
    - unimplemented methods are still logged as an error, but an empty
      result sent back nevertheless
    - the effect can be seen when setting a breakpoint which gets removed
      immediately when the reply is an empty result

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 var InspectorBackendDispatcher = require('./lib/InspectorBackendDispatcher/InspectorBackendDispatcher.js')
   , InspectorPageAgent = require('./lib/inspector/InspectorPageAgent.js')
+  , InspectorConsoleAgent = require('./lib/inspector/InspectorConsoleAgent.js')
+  , InspectorNetworkAgent = require('./lib/inspector/InspectorNetworkAgent.js')
 
 exports.inspectorBackendDispatcher = function inspectorBackendDispatcher() {
   return new InspectorBackendDispatcher({
-    pageAgent: new InspectorPageAgent()
+      pageAgent    : new InspectorPageAgent()
+    , consoleAgent : new InspectorConsoleAgent()
+    , networkAgent : new InspectorNetworkAgent()
   })
 }
