1db89d1578255311035ec1e1041be20aa4544bda
Thorsten Lorenz <thlorenz@gmx.de>
2014-10-15 15:43:58 -0400

    devtools: HeapProfiler that currently ignores all methods

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var InspectorBackendDispatcher = require('./lib/InspectorBackendDispatcher/Inspe
   , InspectorPageAgent = require('./lib/inspector/InspectorPageAgent.js')
   , InspectorConsoleAgent = require('./lib/inspector/InspectorConsoleAgent.js')
   , InspectorNetworkAgent = require('./lib/inspector/InspectorNetworkAgent.js')
+  , InspectorHeapProfilerAgent = require('./lib/inspector/InspectorHeapProfilerAgent.js')
 
 exports.inspectorBackendDispatcher = function inspectorBackendDispatcher() {
   return new InspectorBackendDispatcher({
-      pageAgent    : new InspectorPageAgent()
-    , consoleAgent : new InspectorConsoleAgent()
-    , networkAgent : new InspectorNetworkAgent()
+      pageAgent         : new InspectorPageAgent()
+    , consoleAgent      : new InspectorConsoleAgent()
+    , networkAgent      : new InspectorNetworkAgent()
+    , heapProfilerAgent : new InspectorHeapProfilerAgent()
   })
 }
