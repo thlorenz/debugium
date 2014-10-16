3fab96736e0eeaa6afebccd400496b6e01c2a69b
Thorsten Lorenz <thlorenz@gmx.de>
2014-10-15 22:22:57 -0400

    devtools: DebuggerAgent interface
    - added RuntimeAgent and IndexedDBAgent with limited functionality in
      the process in order to avoid warnings
    - fixed securityOrigin not sent for getResourceTree
    - storing scripts by id
    
    Still encountering the following problems
    
    - we should see Debugger.getScriptSource after replying to
      Page.getResourceContent, but don't
    
    Somehow when setting a breakpoint it is unable to find the breakpoint
    target although it knows about the script and it's source (sends along
    scriptID and got resourceContent of that ID before).
    
    The backend sends back an ok with the info about the breakpoint set and
    at this point the UI tries to add it but has trouble mapping it to a
    UILocation.
    As a result the breakpoint never gets set properly and we are also
    unable to remove it.
    
    Stacktrace:
    
    ```
    Uncaught TypeError: Cannot read property '_rawLocationToUILocation' of null Main.js:formatted:22893
    WebInspector.DebuggerWorkspaceBinding.rawLocationToUILocation Main.js:formatted:22893
    WebInspector.BreakpointManager.TargetBreakpoint._addResolvedLocation Main.js:formatted:26244
    WebInspector.BreakpointManager.TargetBreakpoint._didSetBreakpointInDebugger Main.js:formatted:26219
    didSetBreakpoint Main.js:formatted:22445InspectorBackendClass.AgentPrototype.dispatchResponse Main.js:formatted:5262
    InspectorBackendClass.Connection.dispatch Main.js:formatted:5071
    InspectorBackendClass.WebSocketConnection._onMessage
    ```

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 var InspectorBackendDispatcher = require('./lib/InspectorBackendDispatcher/InspectorBackendDispatcher.js')
-  , InspectorPageAgent = require('./lib/inspector/InspectorPageAgent.js')
-  , InspectorConsoleAgent = require('./lib/inspector/InspectorConsoleAgent.js')
-  , InspectorNetworkAgent = require('./lib/inspector/InspectorNetworkAgent.js')
+  , InspectorPageAgent         = require('./lib/inspector/InspectorPageAgent.js')
+  , InspectorConsoleAgent      = require('./lib/inspector/InspectorConsoleAgent.js')
+  , InspectorNetworkAgent      = require('./lib/inspector/InspectorNetworkAgent.js')
   , InspectorHeapProfilerAgent = require('./lib/inspector/InspectorHeapProfilerAgent.js')
-  , InspectorDebuggerAgent = require('./lib/inspector/InspectorDebuggerAgent.js')
+  , InspectorDebuggerAgent     = require('./lib/inspector/InspectorDebuggerAgent.js')
+  , InspectorRuntimeAgent      = require('./lib/inspector/InspectorRuntimeAgent.js')
+  , InspectorIndexedDBAgent      = require('./lib/inspector/InspectorIndexedDBAgent.js')
 
 exports.inspectorBackendDispatcher = function inspectorBackendDispatcher() {
   return new InspectorBackendDispatcher({
@@ -13,5 +15,7 @@ exports.inspectorBackendDispatcher = function inspectorBackendDispatcher() {
     , networkAgent      : new InspectorNetworkAgent()
     , heapProfilerAgent : new InspectorHeapProfilerAgent()
     , debuggerAgent     : new InspectorDebuggerAgent()
+    , runtimeAgent      : new InspectorRuntimeAgent()
+    , indexedDBAgent      : new InspectorIndexedDBAgent()
   })
 }
